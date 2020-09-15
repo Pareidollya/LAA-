a45d75dc0a6c6e4187d91322dda6c696c04ed605
Pareiollya <63682822+Pareiollya@users.noreply.github.com>
2020-09-15 20:44:01 -0300

    Update Boubble.py

--- Boubble.py
+++ Boubble.py
@@ -2,7 +2,7 @@ vetor = [5,1,4,2,8,3452,76,23,56,657,123,-12345,5,5,66,4,2,23,643,234,3,0]
 
 for j in range(len(vetor)): 
   
-  for i in range((len(vetor)-1)-j):  todo vasculhado, aumentando desempenho. 
+  for i in range((len(vetor)-1)-j): 
 
     num = vetor[i] 
 
